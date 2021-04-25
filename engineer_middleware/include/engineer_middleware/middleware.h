63e43cd5a28b6cf90fc22102fa477d2b30c8fbad
astro <1015926336@qq.com>
2021-04-25 21:16:43 +0800

    package chassis and arm into middleware

--- engineer_middleware/include/engineer_middleware/middleware.h
+++ engineer_middleware/include/engineer_middleware/middleware.h
@@ -10,18 +10,24 @@
 #include <actionlib/server/simple_action_server.h>
 
 #include <rm_msgs/EngineerAction.h>
+
+#include "engineer_middleware/step_queue.h"
+#include "engineer_middleware/chassis.h"
 namespace engineer_middleware {
 class Middleware {
  public:
   Middleware(ros::NodeHandle &nh);
   ros::NodeHandle nh_;
+  //action
   actionlib::SimpleActionServer<rm_msgs::EngineerAction> action_;
   rm_msgs::EngineerFeedback freedback_;
   rm_msgs::EngineerResult result_;
-
+  //chassis
+  Chassis *chassis_;
   void executeCB(const rm_msgs::EngineerGoalConstPtr &goal) {
 
   }
+  void run() const;
 
 };
 }
--- engineer_middleware/src/middleware.cpp
+++ engineer_middleware/src/middleware.cpp
@@ -10,6 +10,18 @@ Middleware::Middleware(ros::NodeHandle &nh) : nh_(nh),
                                                       "Engineer",
                                                       boost::bind(&Middleware::executeCB, this, _1),
                                                       false) {
+  chassis_ = new Chassis(nh);
   action_.start();
+}
+void Middleware::run() const {
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  moveit::planning_interface::MoveGroupInterface arm_group("engineer_arm");
+  moveit::planning_interface::MoveGroupInterface hand_group("engineer_hand");
+  XmlRpc::XmlRpcValue steps_params;
+  nh_.getParam("place", steps_params);
+  engineer_middleware::StepQueue step_queue(steps_params, arm_group, hand_group);
+  step_queue.move();
+  steps_params.clear();
 }
 }
