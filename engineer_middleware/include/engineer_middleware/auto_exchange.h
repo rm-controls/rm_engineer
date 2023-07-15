958318652cf1a62e5986bfcb9d11b792045cf1ac
LSY <2028238109@qq.com>
2023-07-15 09:28:25 +0800

    Decide to introduce commander into auto_exchange.h.

--- engineer_middleware/include/engineer_middleware/auto_exchange.h
+++ engineer_middleware/include/engineer_middleware/auto_exchange.h
@@ -3,6 +3,7 @@
 //
 #pragma once
 #include <rm_common/ros_utilities.h>
+#include <rm_common/decision/command_sender.h>
 
 namespace auto_exchange
 {
@@ -54,8 +55,10 @@ public:
     //      joint1:
     //        offset: 0.01
     //        range: [0.,1.]
+    //        max_vel: 0.03
     //        near_tolerance: 0.03
     offset_ = xmlRpcGetDouble(joint, "offset", 0.);
+    max_vel_ = xmlRpcGetDouble(joint, "max_vel", 1.0);
     near_tolerance_ = xmlRpcGetDouble(joint, "near_tolerance", 0.05);
     ROS_ASSERT(joint["range"].getType() == XmlRpc::XmlRpcValue::TypeArray);
     min_position_ = xmlRpcGetDouble(joint["range"], 0);
@@ -63,10 +66,8 @@ public:
   }
   bool judgeJointPosition()
   {
-    return (abs(current_position_ - offset_ - min_position_) <= near_tolerance_ ||
-            abs(max_position_ + offset_ - current_position_) <= near_tolerance_) ?
-               true :
-               false;
+    return abs(current_position_ - offset_ - min_position_) <= near_tolerance_ ||
+           abs(max_position_ + offset_ - current_position_) <= near_tolerance_;
   }
   int judgeMoveDirect()
   {
@@ -82,10 +83,14 @@ class ProgressBase
 public:
   ProgressBase(XmlRpc::XmlRpcValue& progress)
   {
-    time_out_ = xmlRpcGetDouble(progress["common"], "timeout", 1e10);
+    // progress:
+    //        timeout: 3.
+    time_out_ = xmlRpcGetDouble(progress, "timeout", 1e10);
   }
-  virtual void nextProcess();
-  virtual bool isFinish() = 0;
+  virtual void nextProcess(bool flag);
+  virtual void manageProcess(bool flag);
+  virtual void run();
+  virtual void printProcess();
   bool checkTimeout(ros::Duration period)
   {
     if (period.toSec() > time_out_)
@@ -97,11 +102,59 @@ public:
   }
 
 protected:
+  int process_;
+  bool is_finish_{ false };
   double time_out_{};
+  ros::Time start_time_;
 };
 
-class Find
+class Find : public ProgressBase
 {
+public:
+  Find(XmlRpc::XmlRpcValue& find) : ProgressBase(find)
+  {
+    //        find:
+    //          pitch:
+    //        offset: 0.01
+    //        range: [0.,1.]
+    //        max_vel: 0.03
+    //        near_tolerance: 0.03
+    //          confirm_lock_time
+    process_ = SWING;
+    ROS_ASSERT(find["yaw"].getType() == XmlRpc::XmlRpcValue::TypeStruct);
+    ROS_ASSERT(find["yaw"].getType() == XmlRpc::XmlRpcValue::TypeStruct);
+    yaw_ = new JointInfo(find["yaw"]);
+    pitch_ = new JointInfo(find["pitch_"]);
+    confirm_lock_time_ = xmlRpcGetDouble(find, "confirm_lock_time", 10);
+  }
+  void nextProcess(bool flag) override
+  {
+    process_++;
+    printProcess();
+  }
+  void manageProcess(bool flag) override
+  {
+    if (process_ != LOCKED)
+      nextProcess(flag);
+    else
+      is_finish_ = true;
+  }
+  void printProcess() override
+  {
+    if (process_ == SWING)
+      ROS_INFO_STREAM("SWING");
+    else if (process_ == FOUND)
+      ROS_INFO_STREAM("FOUND");
+    else if (process_ == LOCKED)
+      ROS_INFO_STREAM("LOCKED");
+  }
+  void run() override
+  {
+  }
+
+private:
+  JointInfo *yaw_{}, *pitch_{};
+  double search_angle_{}, confirm_lock_time_{};
 };
 
 class ProAdjust
