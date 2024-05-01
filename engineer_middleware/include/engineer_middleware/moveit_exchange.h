d21e50027671943c50d2b253950655bb4d8fd5f3
cc0h <7921166012@qq.com>
2024-05-01 22:08:07 +0800

    record.

--- engineer_middleware/include/engineer_middleware/moveit_exchange.h
+++ engineer_middleware/include/engineer_middleware/moveit_exchange.h
@@ -74,6 +74,60 @@ public:
     y_.init(config["y"], "y", nh);
     yaw_.init(config["yaw"], "yaw", nh);
   }
+  void init()
+  {
+    state_ = SET_GOAL;
+    initComputeValue();
+  }
+  void stateMachine()
+  {
+    switch (state_)
+    {
+      case SET_GOAL:
+      {
+        setGoal();
+        state_ = ADJUST_X;
+      }
+      break;
+      case ADJUST_X:
+      {
+        computeChassisVel();
+        chassis_vel_cmd_.linear.y = 0.;
+        chassis_vel_cmd_.angular.z = 0.;
+        if (x_.isFinish())
+          state_ = re_adjusted_ ? FINISH : ADJUST_Y;
+      }
+      break;
+      case ADJUST_Y:
+      {
+        computeChassisVel();
+        chassis_vel_cmd_.linear.x = 0.;
+        chassis_vel_cmd_.angular.z = 0.;
+        if (y_.isFinish())
+          state_ = ADJUST_YAW;
+      }
+      break;
+      case ADJUST_YAW:
+      {
+        computeChassisVel();
+        chassis_vel_cmd_.linear.x = 0.;
+        chassis_vel_cmd_.linear.y = 0.;
+        if (yaw_.isFinish())
+        {
+          state_ = SET_GOAL;
+          re_adjusted_ = true;
+        }
+      }
+      break;
+      case FINISH:
+      {
+        is_finish_ = true;
+        re_adjusted_ = false;
+        initComputeValue();
+      }
+      break;
+    }
+  }
 
 private:
   void initComputeValue()
@@ -85,16 +139,52 @@ private:
     chassis_vel_cmd_.angular.y = 0;
     chassis_vel_cmd_.angular.z = 0;
   }
+  void computeChassisVel()
+  {
+    geometry_msgs::TransformStamped current;
+    current = tf_buffer_.lookupTransform("map", "base_link", ros::Time(0));
+    x_.error = chassis_target_.pose.position.x - current.transform.translation.x;
+    y_.error = chassis_target_.pose.position.y - current.transform.translation.y;
+    double roll, pitch, yaw_current, yaw_goal;
+    quatToRPY(current.transform.rotation, roll, pitch, yaw_current);
+    quatToRPY(chassis_target_.pose.orientation, roll, pitch, yaw_goal);
+    yaw_.error = angles::shortest_angular_distance(yaw_current, yaw_goal);
 
+    ros::Duration dt = ros::Time::now() - last_time_;
+    chassis_vel_cmd_.linear.x = x_.computerVel(dt);
+    chassis_vel_cmd_.linear.y = y_.computerVel(dt);
+    chassis_vel_cmd_.angular.z = yaw_.computerVel(dt);
+
+    last_time_ = ros::Time::now();
+  }
+  void setGoal()
+  {
+    geometry_msgs::TransformStamped base2exchange;
+    double roll, pitch, yaw;
+    base2exchange = tf_buffer_.lookupTransform("base_link", "exchanger", ros::Time(0));
+    quatToRPY(base2exchange.transform.rotation, roll, pitch, yaw);
+
+    double goal_x = base2exchange.transform.translation.x - x_.offset_refer_exchanger;
+    double goal_y = base2exchange.transform.translation.y - y_.offset_refer_exchanger;
+    double goal_yaw = yaw * yaw_.offset_refer_exchanger;
+    chassis_original_target_.pose.position.x = goal_x;
+    chassis_original_target_.pose.position.y = goal_y;
+
+    tf2::Quaternion quat_tf;
+    quat_tf.setRPY(0, 0, goal_yaw);
+    geometry_msgs::Quaternion quat_msg = tf2::toMsg(quat_tf);
+    chassis_original_target_.pose.orientation = quat_msg;
+    chassis_target_ = chassis_original_target_;
+    tf2::doTransform(chassis_target_, chassis_target_, tf_buffer_.lookupTransform("map", "base_link", ros::Time(0)));
+  }
+  bool re_adjusted_{ false }, is_finish_{ false };
   AdjustStates state_, last_state_;
   tf2_ros::Buffer& tf_buffer_;
   ros::NodeHandle nh_{};
+  ros::Time last_time_;
   SingleDirectionMove x_, y_, yaw_;
   geometry_msgs::Twist chassis_vel_cmd_{};
-};
-
-class FindExchanger
-{
+  geometry_msgs::PoseStamped chassis_target_{}, chassis_original_target_{};
 };
 
 class MoveitExchange
